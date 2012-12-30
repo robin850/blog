193f0339af4b4f9815ac4424e3c55bea81aeb0fb
Robin Dupret <robin.dupret@gmail.com>
2012-12-30 18:32:43 +0100

    Fix syntax error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,7 @@ class Comment < ActiveRecord::Base
     (self.admin == true) ? true : false
   end
 
-  validates :body, :presence => true, :message => "Vous devez spécifier un message."
-  validates :author, :presence => true, :message => "Vous devez renseigner votre nom."
-  validates :email, :presence => true, :message => "Vous devez spéicifier votre e-mail."
+  validates_presence_of :body, :message => "Vous devez spécifier un message."
+  validates_presence_of :author, :message => "Vous devez renseigner votre nom."
+  validates_presence_of :email, :message => "Vous devez spéicifier votre e-mail."
 end
