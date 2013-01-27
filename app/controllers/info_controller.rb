33476d065d71c362feddbd67c0b5c597511cdff9
Robin Dupret <robin.dupret@gmail.com>
2013-01-27 17:54:05 +0100

    Fix redirecting and a flash message
    Encode the file with utf-8 to prevent Heroku to crash

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class InfoController < ApplicationController
 
   def about
@@ -12,6 +13,8 @@ class InfoController < ApplicationController
       @message = Message.new(params[:message])
       if @message.valid?
         ContactMailer.notification(@message).deliver
+        flash[:notice] = "Merci, votre message a été envoyé !"
+        redirect_to contact_path
       else
         render :contact
       end
